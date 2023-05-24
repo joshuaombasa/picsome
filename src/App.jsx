340bf8ce105ec34b52d40435d470cf9629e9c8cd
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-24 18:31:55 +0300

    added jsx

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Header from './components/Header'
+import './App.css'
+import Cart from './pages/Cart'
+import Photos from './pages/Photos'
+
+function App() {
+  
+  return (
+    <div className='app--container'>
+    <Header/>
+
+    <Routes>
+      <Route exact path='/' element={<Photos/>}></Route>
+      <Route path='/cart' element={<Cart/>}></Route>
+    </Routes>
+    </div>
+  )
+}
+
+export default App
