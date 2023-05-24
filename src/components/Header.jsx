d2d2499b2286db9c8d4d0b9db09747b876cfd9c8
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-24 18:30:52 +0300

    added jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React, {useContext} from "react";
+import { Link } from "react-router-dom";
+import { cartContext } from "../CartContext";
+
+export default function Header() {
+
+    const { cartItemsDataArray } = useContext(cartContext)
+
+    const iconToDisplay = cartItemsDataArray.length > 0 ? "ri-shopping-cart-fill" : "ri-shopping-cart-line"
+
+    return (
+        <header>
+            <h1 className="logo--title">
+                <Link to="/">picSome</Link>
+            </h1>
+            <Link to="/cart">
+                <i className={`${iconToDisplay}`}></i>
+            </Link>
+        </header>
+    )
+}
