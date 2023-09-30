bd4a397186c2ace2048e2f926139b074aadcf0bd
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-09-30 21:12:56 +0300

    updated app to use new router

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import {CartContextProvider} from "./CartContext"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <CartContextProvider>
-     <BrowserRouter>
         <React.StrictMode>
            <App />
         </React.StrictMode>
-     </BrowserRouter>
   </CartContextProvider>
 )
